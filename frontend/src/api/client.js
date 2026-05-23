89c7861d4a0c6669a202a61cfed39bc2da282f69
mubirusamuelalizeyuna-cloud <mubirusamuelalizeyuna@gmail.com>
2026-05-23 08:16:09 +0300

    sam

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = import.meta.env.VITE_API_URL;
-const API_BASE_URL = 'https://qrcode-generator-z19r.onrender.com/api';
+const API_BASE_URL = 'https://qrcode-generator-z19r.onrender.com';
 
 const api = axios.create({ baseURL: API_BASE_URL });
 
